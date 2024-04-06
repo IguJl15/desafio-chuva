4b7a832f2ed78493c366a24e1be398eb0ed2bf4b
Igor julliano <igor.julliano2@gmail.com>
2024-04-05 22:39:37 -0300

    refactor: enhance activity card widget

--- dart/lib/ui/home_page/components/activity_card.dart
+++ dart/lib/ui/home_page/components/activity_card.dart
@@ -1,5 +1,7 @@
-import 'package:chuva_dart/models/activity.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+
+import '../../../models/activity.dart';
 
 class ActivityCard extends StatelessWidget {
   final Activity activity;
@@ -11,16 +13,54 @@ class ActivityCard extends StatelessWidget {
     final startTime = TimeOfDay.fromDateTime(activity.start);
     final endTime = TimeOfDay.fromDateTime(activity.end);
 
+    final borderColorHex = 0xFF000000 | (int.tryParse(activity.category.color?.substring(1) ?? "", radix: 16) ?? 0);
+
+    final theme = Theme.of(context);
+    final textTheme = theme.textTheme;
+    final colorScheme = theme.colorScheme;
+
+    // TODO: Check for sub activities
     return Card(
-      elevation: 1,
+      elevation: 4,
+      clipBehavior: Clip.hardEdge,
+      surfaceTintColor: Colors.transparent,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(2)),
       child: InkWell(
         onTap: () {},
-        child: Column(
-          children: [
-            Text("$type de $startTime até $endTime"),
-            Text(activity.title.value),
-            Text(activity.people.join(", ")),
-          ],
+        child: Container(
+          padding: const EdgeInsets.only(left: 8) + const EdgeInsets.all(4),
+          decoration: BoxDecoration(
+            border: Border(
+              left: BorderSide(
+                width: 4,
+                style: BorderStyle.solid,
+                color: Color(borderColorHex),
+              ),
+            ),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Flexible(
+                    child: Text(
+                      "$type de ${startTime.format(context)} até ${endTime.format(context)}",
+                      style: textTheme.labelMedium,
+                    ),
+                  ),
+                  Icon(Icons.bookmark_rounded, color: colorScheme.tertiary)
+                ],
+              ),
+              Text(activity.title.value, style: textTheme.bodyLarge),
+              Text(
+                activity.people.map((p) => p.name.trim()).join(", "),
+                style: textTheme.bodyMedium?.copyWith(color: Theme.of(context).colorScheme.secondary),
+              ),
+              if (kDebugMode && activity.parent != null) Text("Parent: ${activity.parent}", style: textTheme.bodySmall),
+            ],
+          ),
         ),
       ),
     );
